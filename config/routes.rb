ad306cd89e37bf19e6031cf1e3492c48c64f15b3
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-03-02 12:57:15 +0000

    [Update] Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'books#index'
+  devise_for :users
+  resources :books, only: [:index, :show, :edit, :create, :destroy]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
