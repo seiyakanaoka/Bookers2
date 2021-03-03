9619e6e5d880bd800e2ade88bce0fec68059ddad
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-03-03 14:59:23 +0000

    [Update]Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
 
   def show
    @book = Book.find(params[:id]) 
+   @user = @book.user
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'books#index'
+  root 'users#index'
   devise_for :users
   resources :books, only: [:index, :show, :edit, :create, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
