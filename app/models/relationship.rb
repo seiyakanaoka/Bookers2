1c33880a257367128c5c30add054156b81de7c70
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-04-03 07:21:23 +0000

    応用課題６

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,4 @@
 class Relationship < ApplicationRecord
-  belongs_to :follower, class_name: "User"
-  belongs_to :followed, class_name: "User"
+  belongs_to :follower, class_name: "User" #フォローしている側のidを管理
+  belongs_to :followed, class_name: "User" #フォローした相手のidを管理
 end
